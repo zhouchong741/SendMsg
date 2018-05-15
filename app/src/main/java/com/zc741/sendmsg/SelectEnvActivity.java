f71e5444e5a81b860b424165ea25cdc1ef969fdf
zhouchong <zhouchong741@hotmail.com>
2018-05-15 10:06:37 +0800

    1. 增加权限

--- app/src/main/java/com/zc741/sendmsg/SelectEnvActivity.java
+++ app/src/main/java/com/zc741/sendmsg/SelectEnvActivity.java
@@ -3,7 +3,9 @@ package com.zc741.sendmsg;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +19,9 @@ import com.zc741.sendmsg.http.HttpUrls;
 public class SelectEnvActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_MESSAGE = 1;
+    private static final int REQUEST_CODE = 2;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +33,8 @@ public class SelectEnvActivity extends AppCompatActivity implements View.OnClick
         testBtn.setOnClickListener(this);
         serverBtn.setOnClickListener(this);
 
-        isPermission();
+//        isPermission();
+        requestMultiplePermissions();
     }
 
     @Override
@@ -55,16 +60,32 @@ public class SelectEnvActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestMultiplePermissions() {
+        String[] permissions = {
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.READ_PHONE_STATE
+        };
+        requestPermissions(permissions,REQUEST_CODE);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == MY_PERMISSIONS_REQUEST_SEND_MESSAGE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//        if (requestCode == MY_PERMISSIONS_REQUEST_SEND_MESSAGE) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                System.out.println("已授权");
+//            } else {
+//                Toast.makeText(this, "发送短信权限获取失败，请重新获取", Toast.LENGTH_SHORT).show();
+//            }
+//            return;
+//        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 System.out.println("已授权");
             } else {
-                Toast.makeText(this, "发送短信权限获取失败，请重新获取", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "权限获取失败，请重新获取", Toast.LENGTH_SHORT).show();
             }
-            return;
         }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
